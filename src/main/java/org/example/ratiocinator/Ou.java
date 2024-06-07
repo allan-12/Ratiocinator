dd3a91526ad5a9f48793d868fdeee04e6d6642b2
Allan-12 <hei.allan.3@gmail.com>
2024-06-07 16:32:57 +0300

    feat: added a Ou class

---
+++ src/main/java/org/example/ratiocinator/Ou.java
@@ -0,0 +1,16 @@
+package org.example.ratiocinator;
+
+public class Ou extends  Affirmation{
+    private Affirmation af1;
+    private  Affirmation af2;
+
+    public Ou(Affirmation af1, Affirmation af2){
+        this.af1= af1;
+        this.af2= af2;
+    }
+    @Override
+    public boolean evaluer(){
+        return af1.evaluer() || af2.evaluer();
+
+    }
+}
