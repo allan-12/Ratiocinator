70d4c1cb0df4a9e5ca232ee1f91c28fb705af123
Allan-12 <hei.allan.3@gmail.com>
2024-06-07 16:30:22 +0300

    feat: added a Et class for the affirmation

---
+++ src/main/java/org/example/ratiocinator/Et.java
@@ -0,0 +1,15 @@
+package org.example.ratiocinator;
+
+public class Et extends Affirmation{
+    private Affirmation af1;
+    private Affirmation af2;
+
+    public Et (Affirmation af1 , Affirmation af2){
+        this.af1 = af1;
+        this.af2 = af2;
+    }
+    @Override
+    public boolean evaluer(){
+        return af1.evaluer() && af2.evaluer();
+    }
+}
